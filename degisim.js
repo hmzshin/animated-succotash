63bf263056f7a65226c74b90dbe7cba9d0daf28e
hmzshin <hamzasahin@outlook.com>
2023-09-28 00:00:30 +0300

    first setup

---
+++ degisim.js
@@ -0,0 +1,35 @@
+const degisim = [
+  {
+    letter: "a",
+    changeWith: 1,
+  },
+  {
+    letter: "e",
+    changeWith: 2,
+  },
+  {
+    letter: "ı",
+    changeWith: 3,
+  },
+  {
+    letter: "i",
+    changeWith: 4,
+  },
+  {
+    letter: "o",
+    changeWith: 5,
+  },
+  {
+    letter: "ö",
+    changeWith: 6,
+  },
+  {
+    letter: "u",
+    changeWith: 7,
+  },
+  {
+    letter: "ü",
+    changeWith: 8,
+  },
+];
+module.exports = { degisim };
